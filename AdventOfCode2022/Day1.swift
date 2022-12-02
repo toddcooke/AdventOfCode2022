661bbf246162ef11411e37ef010aa9345575fab0
ToddCooke <toddcookevt@gmail.com>
2022-12-02 18:04:19 -0500

    day2part2

--- AdventOfCode2022/Day1.swift
+++ AdventOfCode2022/Day1.swift
@@ -23,3 +23,31 @@ func day1() -> Int {
 
     return mostCals
 }
+
+func day1Part2() -> Int {
+    var first = -1
+    var second = -1
+    var third = -1
+
+    let contents = try! String(contentsOfFile: adventDir + "day1.txt")
+    let elves = contents.split(separator: "\n\n")
+
+    for elf in elves {
+        var total = 0
+        for calorie in elf.split(separator: "\n") {
+            total += Int(calorie)!
+        }
+        if total > first {
+            third = second
+            second = first
+            first = total
+        } else if total > second {
+            third = second
+            second = total
+        } else if total > third {
+            third = total
+        }
+    }
+
+    return [first, second, third].reduce(0, +)
+}
--- AdventOfCode2022/Day2.swift
+++ AdventOfCode2022/Day2.swift
@@ -15,6 +15,18 @@ protocol Material {
 // 3 if draw
 // 6 if win
 extension Material {
+    func toGameResult() -> GameResult {
+        if self is Rock {
+            return GameResult.X
+        } else if self is Paper {
+            return GameResult.Y
+        } else if self is Scissors {
+            return GameResult.Z
+        }
+        print("missing case in togameresult")
+        exit(1)
+    }
+
     func versus(_ other: Material) -> Int {
         if self is Rock && other is Rock {
             return 1 + 3
@@ -81,4 +93,68 @@ func day2() -> Int {
         score += result
     }
     return score
+}
+
+
+func day2Part2() -> Int {
+    var score = 0
+    let contents = try! String(contentsOfFile: adventDir + "day2.txt")
+    let rounds = contents.split(separator: "\n")
+
+    for round in rounds {
+        let opponentAndSelf = round.split(separator: " ")
+        let opponent = String(opponentAndSelf[0]).toMaterial()
+        let me = String(opponentAndSelf[1]).toMaterial()
+        let result = solveRound(otherMaterial: opponent, gameResult: me.toGameResult())
+        score += result
+    }
+    return score
+}
+
+enum GameResult {
+    case X
+    case Y
+    case Z
+}
+
+/// 1 for rock
+/// 2 for paper
+/// 3 for scissors
+/// 0 if loss
+/// 3 if draw
+/// 6 if win
+/// X means I lose
+/// Y means draw
+/// Z means I win
+/// A Y = draw   1 + 3 = 4
+/// B X = i lose 1 + 0 = 1
+/// C Z = i win  1 + 6 = 7
+// Returns round total
+func solveRound(otherMaterial: Material, gameResult: GameResult) -> Int {
+    if otherMaterial is Rock && gameResult == GameResult.Y {
+        return 1 + 3 // draw
+    } else if otherMaterial is Rock && gameResult == GameResult.X {
+        return 3 + 0 // lose
+    } else if otherMaterial is Rock && gameResult == GameResult.Z {
+        return 2 + 6 // win
+    }
+
+    if otherMaterial is Paper && gameResult == GameResult.Y {
+        return 2 + 3 // draw
+    } else if otherMaterial is Paper && gameResult == GameResult.X {
+        return 1 + 0 // lose
+    } else if otherMaterial is Paper && gameResult == GameResult.Z {
+        return 3 + 6 // win
+    }
+
+    if otherMaterial is Scissors && gameResult == GameResult.Y {
+        return 3 + 3 // draw
+    } else if otherMaterial is Scissors && gameResult == GameResult.X {
+        return 2 + 0 // lose
+    } else if otherMaterial is Scissors && gameResult == GameResult.Z {
+        return 1 + 6 // win
+    }
+
+    print("missed case in solveRound")
+    exit(1)
 }
