b29a024a17f979e9c74f1d7760a7da769bc03eb9
ToddCooke <toddcookevt@gmail.com>
2022-12-08 12:10:01 -0500

    WIP-day8 worked for small example, too high for full input. Not sure yet...

--- AdventOfCode2022/Day8.swift
+++ AdventOfCode2022/Day8.swift
@@ -11,69 +11,39 @@ func day8() -> Int {
     var visibleTrees: [[Int]] = []
 
     // Create array of trees
-    for (i, line) in lines.enumerated() {
+    for (_, line) in lines.enumerated() {
         var lineOfTrees: [Int] = []
-        for (j, tree) in line.enumerated() {
+        for (_, tree) in line.enumerated() {
             lineOfTrees.append(Int(String(tree))!)
         }
         trees.append(lineOfTrees)
-        visibleTrees.append(lineOfTrees.map {
-            $0 * 0
+        visibleTrees.append(lineOfTrees.map { element in
+            1
         })
     }
 
-    // Count visible trees
+//    print(trees[2])
+//    print(isTreeHidden(treeIndex: 0, row: trees[2]))
+//    print(isTreeHidden(treeIndex: 1, row: trees[2]))
+//    print(isTreeHidden(treeIndex: 2, row: trees[2]))
+//    print(isTreeHidden(treeIndex: 3, row: trees[2]))
+//    print(isTreeHidden(treeIndex: 4, row: trees[2]))
+//    exit(1)
+
+
+    // Find visible trees
     for (i, line) in trees.enumerated() {
-        for (j, tree) in line.enumerated() {
-            var row = i
-            var col = j
-
-            // go down row
-            while row < trees.count {
-                print(row, col)
-                if trees[row][col] < trees[i][j] {
-                    visibleTrees[row][col] = 1
-                    break
-                }
-                row += 1
+        for (j, _) in line.enumerated() {
+            // if row and col hidden: visible[i][j] = 1
+            let rowHidden = isTreeHidden(treeIndex: i, row: line)
+            let colHidden = isTreeHidden(treeIndex: j, row: columnToArray(col: j, arr: trees))
+            if rowHidden && colHidden {
+                visibleTrees[i][j] = 0
             }
-            row = i
-
-            // go up row
-            while row < trees.count {
-                print(row, col)
-                if trees[row][col] < trees[i][j] {
-                    visibleTrees[row][col] = 1
-                    break
-                }
-                row -= 1
-            }
-            row = i
-
-            // go right across col
-            while col < trees[0].count {
-                print(row, col)
-                if trees[row][col] < trees[i][j] {
-                    visibleTrees[row][col] = 1
-                    break
-                }
-                col += 1
-            }
-            col = j
-
-            // go left across col
-            while col >= 0 {
-                print(row, col)
-                if trees[row][col] < trees[i][j] {
-                    visibleTrees[row][col] = 1
-                    break
-                }
-                col -= 1
-            }
-            col = j
         }
     }
 
+    // Count visible trees
     var total = 0
     for (i, line) in visibleTrees.enumerated() {
         for (j, tree) in line.enumerated() {
@@ -87,5 +57,38 @@ func day8() -> Int {
             }
         }
     }
+    print(visibleTrees)
     return total
+}
+
+func isTreeHidden(treeIndex: Int, row: [Int]) -> Bool {
+    if treeIndex == 0 || treeIndex == row.count - 1 {
+        return false
+    }
+    let tree = row[treeIndex]
+    let leftOfTree = row[0...treeIndex - 1]
+    let rightOfTree = row[treeIndex + 1...row.count - 1]
+//    print("tree:\(row[treeIndex]) left of tree:\(leftOfTree) rightOfTree:\(rightOfTree)")
+    if leftOfTree.max()! >= tree && rightOfTree.max()! >= tree {
+        return true
+    } else {
+        return false
+    }
+}
+
+
+/// col = 1
+/// arr = [[1,2],[3,4]]
+/// ret [2,4]
+func columnToArray(col: Int, arr: [[Int]]) -> [Int] {
+    var result: [Int] = []
+
+    for i in arr {
+        for (j, item) in i.enumerated() {
+            if j == col {
+                result.append(item)
+            }
+        }
+    }
+    return result
 }
